7ef566c95707afb29232d6043dd19cafec2dc01e
spin6lock <spin6lock@users.noreply.github.com>
2019-10-21 15:10:20 +0800

    wip

---
+++ README.md
@@ -0,0 +1,85 @@
+socks5协议实现
+=============
+先实现CONNECT
+[rfc](https://www.ietf.org/rfc/rfc1928.txt)
+
+交互流程
+=======
+认证阶段：
+
+Client->Server:
++----+----------+----------+
+|VER | NMETHODS | METHODS  |
++----+----------+----------+
+| 1  |    1     |  1~255   |
++----+----------+----------+
+
+VER 字段是当前协议的版本号，也就是 5； NMETHODS 字段是 METHODS 字段占用的字节数； METHODS 字段的每一个字节表示一种认证方式，表示客户端支持的全部认证方式。
+
+	0x00: NO AUTHENTICATION REQUIRED
+	0x01: GSSAPI
+	0x02: USERNAME/PASSWORD
+	0x03: to X’7F’ IANA ASSIGNED
+	0x80: to X’FE’ RESERVED FOR PRIVATE METHODS
+	0xFF: NO ACCEPTABLE METHODS
+
+Server -> Client:
+    +----+--------+
+    |VER | METHOD |
+    +----+--------+
+    | 1  |   1    |
+    +----+--------+
+0x05 0x00：告诉客户端采用无认证的方式建立连接；
+0x05 0xff：客户端的任意一种认证方式服务器都不支持。
+
+连接阶段:
+
+Client -> Server:
+    +----+-----+-------+------+----------+----------+
+    |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+    +----+-----+-------+------+----------+----------+
+    | 1  |  1  |   1   |  1   | Variable |    2     |
+    +----+-----+-------+------+----------+----------+
+    CMD 字段 command 的缩写：
+
+    * 0x01：CONNECT 建立 TCP 连接
+    * 0x02: BIND 上报反向连接地址
+    * 0x03：关联 UDP 请求
+
+    RSV 字段：保留字段，值为 0x00 ATYP 字段：address type 的缩写，取值为：
+
+    * 0x01：IPv4        
+    * 0x03：域名
+    * 0x04：IPv6
+
+    DST.ADDR 字段：destination address 的缩写，取值随 ATYP 变化：
+
+    * ATYP == 0x01：4 个字节的 IPv4 地址
+    * ATYP == 0x03：1 个字节表示域名长度，紧随其后的是对应的域名
+    * ATYP == 0x04：16 个字节的 IPv6 地址
+    * DST.PORT 字段：目的服务器的端口
+
+Server -> Client:
+    +----+-----+-------+------+----------+----------+
+    |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+    +----+-----+-------+------+----------+----------+
+    | 1  |  1  |   1   |  1   | Variable |    2     |
+    +----+-----+-------+------+----------+----------+
+     REP 字段
+
+    * X'00' succeeded
+    * X'01' general SOCKS server failure
+    * X'02' connection not allowed by ruleset
+    * X'03' Network unreachable
+    * X'04' Host unreachable
+    * X'05' Connection refused
+    * X'06' TTL expired
+    * X'07' Command not supported
+    * X'08' Address type not supported
+    * X'09' to X'FF' unassigned
+
+传输阶段：
+
+    client -> server -> remote
+    remote -> server -> client
+    ...    
--- main.py
+++ main.py
@@ -1,18 +1,82 @@
 #encoding:utf8
+import sys
 from socket import socket, AF_INET, SOCK_STREAM
+from enum import Enum
 
-addr = ("localhost", 12580)
+class Result(Enum):
+    SUCC = 0
+    FAIL = 1
 
+def read_exact(sock, size):
+    result = sock.recv(size)
+    len_result = len(result)
+    while len_result < size:
+        new_result = sock.recv(size - len_result)
+        result = result + new_result
+        len_result = len(result)
+    return result
+
+method_meaning = {
+    bytes.fromhex("00"): "no auth require",
+    bytes.fromhex("01"): "GSSAPI",
+    bytes.fromhex("02"): "USERNAME/PASSWORD",
+}
+def explain_client_accept_auth_method(methods):
+    for byte in methods:
+        print(method_meaning.get(byte, "unknown method"))
+
+def auth(sock):
+    version = sock.recv(1)
+    if version != bytes.fromhex('05'):
+        print("socks version not match! got:", version)
+        return Result.FAIL
+    nmethods_str = sock.recv(1)
+    nmethods = int(nmethods_str, 16)
+    result = read_exact(sock, nmethods)
+    explain_client_accept_auth_method(result) 
+    #only support no password
+    sock.write(bytes.fromhex("0500"))
+    return Result.SUCC
+
+def connect(sock, client_addr):
+    ver = sock.recv(1)
+    if version != bytes.fromhex('05'):
+        print("socks version not match! got:", version)
+        return Result.FAIL
+    cmd = sock.recv(1)
+    if cmd != bytes.fromhex('01'):
+        #TODO add more cmd
+        print("cmd not allow:", cmd)
+        return Result.FAIL
+    rsv = sock.recv(1)  # omitted
+    addr_type = sock.recv(1)
+    #01:ipv4 03:域名
+    if addr_type != bytes.fromhex('01') or addr_type != bytes.fromhex('03'):
+        print("addr_type not allow:", addr_type)
+        return Result.FAIL
+    if addr_type == bytes.fromhex('01'):
+        ipv4_addr = read_exact(4)
+    
+    
 def echo_handler(client_sock, client_addr):
     print("recv conn from {}".format(client_addr))
+    ret = auth(client_sock)
+    if ret != Result.SUCC:
+        print("auth failed")
+        return
+    ret, remote_sock = connect(client_sock)
+    if ret != Result.SUCC:
+        print("connect failed")
+        return
     while 1:
         msg = client_sock.recv(1024)
         if not msg:
             break
-        client_sock.sendall(msg)
+        remote_sock.sendall(msg)
     print(client_addr, "connection close")
     client_sock.close()
 
+addr = ("localhost", 12580)
 def echo_server(addr, backlog=5):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind(addr)
